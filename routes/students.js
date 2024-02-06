f30a611c84499d5b909d0d13a1e55b163cb455e1
cvejicmilos <mlscvjc@gmail.com>
2024-02-06 17:19:49 +0100

    Fixed Back button

--- routes/students.js
+++ routes/students.js
@@ -6,5 +6,6 @@ const studentsController = require('../controllers/studentsController');
 router.post('/class', studentsController.showClass);
 router.post('/studentActivity', studentsController.showStudentActivity);
 router.get('/', studentsController.showMainPage);
+router.post('/', studentsController.showMainPage);
 
 module.exports = router;
